4af618bf9b447c6fd188bb9d133071710dd789b9
Garrett Oreilly <garrettoreilly@icloud.com>
2015-03-12 02:05:19 -0400

    Needed to readd the new lines that `lines` removed
    Without new lines included in the program sent to the lexer, a character
    list cannot be determined invalid or not.

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,13 @@
 import Data.List
 import Control.Monad
+import Control.Applicative
 import Lexer
 import Parser
 
 main :: IO ()
 main = do
     input <- getContents
-    let inputList = concatMap (\(a, x) -> [(a, b, c) | (b, c) <- zip [1..] x]) $ zip [1..] $ lines input
+    let inputList = concatMap (\(a, x) -> [(a, b, c) | (b, c) <- zip [1..] x]) $ zip [1..] $ (++) <$> lines input <*> ["\n"]
     let tokenList = lexProgram inputList
     if kind (last tokenList) == Warning
         then do
