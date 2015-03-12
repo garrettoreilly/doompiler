4d64b3a6669fe368cb701669fc56a92fb10043e2
Garrett Oreilly <garrettoreilly@icloud.com>
2015-03-12 01:41:11 -0400

    Need getContents for multiline files. I totally knew that.

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,7 @@ import Parser
 
 main :: IO ()
 main = do
-    input <- getLine
+    input <- getContents
     let inputList = concatMap (\(a, x) -> [(a, b, c) | (b, c) <- zip [1..] x]) $ zip [1..] $ lines input
     let tokenList = lexProgram inputList
     if kind (last tokenList) == Warning
